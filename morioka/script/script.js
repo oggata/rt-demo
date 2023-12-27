47a954813021f60e6be217ab93eefdb8c10923a4
oggata <oggata@gmail.com>
2023-12-27 19:15:05 +0900

    update

--- morioka/script/script.js
+++ morioka/script/script.js
@@ -138,3 +138,18 @@ function omikuji() {
     isOmikuji = true;
   }
 }
+
+var type = getParam('type');
+if(type==1){
+  $('#NFTModal').modal('show')
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
